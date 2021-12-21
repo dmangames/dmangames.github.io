9dfabf30a00e99752ec51f5b943d7c9bde301344
dmangames <talktodmangames@gmail.com>
2021-12-21 12:42:03 -0600

    wiped

--- src/set3Images.js
+++
@@ -1,9 +0,0 @@
-const set3Images = [
-  { id: 1, src: '/assets/set3/ahri.png', title: 'ahri' },
-  { id: 2, src: '/assets/set3/annie.png', title: 'annie' },
-  { id: 3, src: '/assets/set3/ashe.png', title: 'ashe' },
-  { id: 4, src: '/assets/set3/aurelion-sol.png', title: 'aurelion-sol' },
-  { id: 5, src: '/assets/set3/blitzcrank.png', title: 'blitzcrank' },
-]
-
-export default set3Images;
